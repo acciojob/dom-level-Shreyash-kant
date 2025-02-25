84f741d98935a3e96703492e13951ae05c207878
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-02-25 23:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+let count=1;
+while(elemet.localName!=="html"){
+	++count;
+	element = element.parentNode;
+}
+window.alert(++count);
