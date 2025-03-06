4983386eddfb0e6af35342dcc4ce81c8d747aba6
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-03-06 21:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 const element = document.getElementById("level");
-let count=1;
+let count=0;
 while(elemet.localName!=="html"){
 	++count;
 	element = element.parentNode;
 }
-window.alert(++count);
+window.alert("The level of the element is:"+ ++count);
